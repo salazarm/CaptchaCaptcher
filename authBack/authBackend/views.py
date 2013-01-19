762eccc805f678b05740c650d4f2d1aa76032796
Prabhav Jain <prabhav@seas138.apng.seas.upenn.edu>
2013-01-19 15:53:04 -0500

    First working version....you can track video, and it will tell you if you got the code right or wrong

--- authBack/authBackend/views.py
+++ authBack/authBackend/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import render_to_response
 from django.template import RequestContext, Context
 import random
+from django.utils import simplejson
+import json
+import logging
+import time
 
 ## Imports needed to run 
 from PIL import Image, ImageFont, ImageDraw 
 import math 
 
-## Making clock drawing function based on combineImage.py
 def drawLines(numbers,width,height,r,x,y,saveLoc):
     n = len(numbers) # init vars 
     new_im = Image.new('RGBA', (width,height),(255,255,255,0)) # draw canvas 
@@ -31,14 +34,35 @@ def drawLines(numbers,width,height,r,x,y,saveLoc):
     for i in range(n): 
         draw.text((x+r*math.cos(angles[i]+math.pi/2.0),y+r*math.sin(angles[i]+math.pi/2.0)),str(numbers[i]),fill='blue')
 
-    png_info = new_im.info
-    new_im.save(saveLoc, **png_info)
+    new_im.save(saveLoc)
+    #new_im.show()
+
+    tuples = zip(numbers,angles)
+    d = dict()
+    for t in range(len(tuples)): 
+        d[tuples[t][0]] = (tuples[t][1]-math.pi)*50/math.pi
+    return d 
     
 
 
  
 
 def generate(request):
+    
+    errorDisp = 3
+    TIMEOUT = 5
+    if request.method == "POST":
+        #here is where u verify
+
+        avg = float(request.POST.get('avg'))
+        print "=========================" + str(avg)
+
+        if time.time() - request.session['time'] <= TIMEOUT:
+            if abs(avg - request.session['ans-offset']) < errorDisp:
+                return HttpResponseRedirect('/win')
+            else:
+                return HttpResponseRedirect('/fail')
+
     charSet = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20', '21','22','23','24','25', 'a','b','c','d','e','f''g']
     
     randSet = []
@@ -69,19 +93,9 @@ def generate(request):
 
     #calculate offset
     request.session['ans-offset'] = angles[ans]
-
+    request.session['time'] = time.time()
+    print "=========================" + str(angles[ans])
    
-
-
-    errorDisp = 5
-    if request.is_ajax():
-        #here is where u verify
-
-        avg = float(request.POST.get('avg'))
-
-        if abs(avg - request.session['ans-offset']) < errorDisp:
-            json = simplejson.dumps( [{'status': '1'}])
-            return HttpResponse(json, 'application/javascript')
 
 
             
